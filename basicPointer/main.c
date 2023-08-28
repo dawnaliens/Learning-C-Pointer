42944f5dd3a9783056e698b59972b3683cb7aa77
Richard Liu <richardliux23@gmail.com>
2023-08-28 14:52:42 +0800

    Basic try on pointer to modify values

---
+++ basicPointer/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num = 5;
+    int another = 10;
+    int *numPtr;
+
+    printf("&num - %p\n", (void*)&num);
+    printf("&another - %p\n", (void*)&another);
+    printf("&numPtr - %p\n", (void*)&numPtr);
+
+    // Assign the address of num to numPtr
+    numPtr = &num;
+    printf("numPtr - %p\n", (void*)numPtr);
+
+    printf("*numPtr - %d\n", *numPtr);
+
+    // Modify the value of num via numPtr
+    *numPtr = 100;
+    printf("num - %d\n", num);
+    return 0;
+}

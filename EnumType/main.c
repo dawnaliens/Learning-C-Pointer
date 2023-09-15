96696f1445df82c5d9d1c4f3cf0ffd735546cf91
Richard Liu <richardliux23@gmail.com>
2023-09-15 15:15:50 +0800

    Add one more enum

--- EnumType/main.c
+++ EnumType/main.c
@@ -6,6 +6,43 @@ enum Name{
     Richard,
 };
 
+enum threat_level_t {
+  LOW,
+  GUARDED,
+  ELEVATED,
+  HIGH,
+  SEVERE
+};
+
+void printThreat(enum threat_level_t threat){
+  switch(threat) {
+    case LOW:
+      printf("Green/Low.\n"); 
+      break;
+    case GUARDED:
+      printf("Blue/Guarded.\n"); 
+      break;
+    case ELEVATED:
+      printf("Yellow/Elevated.\n"); 
+      break;
+    case HIGH:
+      printf("Orange/High.\n"); 
+      break;
+    case SEVERE:
+      printf("Red/Severe.\n");
+      break;
+  } 
+};
+
+void printShoes(enum threat_level_t currThreat) {
+  if (currThreat >= ELEVATED) {
+     printf("Please take off your shoes.\n");
+  }
+  else {
+     printf("Please leave your shoes on.\n");
+  }
+}
+
 int main(void){
     enum Name myName;
 
@@ -16,4 +53,10 @@ int main(void){
     }else{
         printf("myName is not Richard.\n");
     }
+
+    enum threat_level_t myThreat = HIGH;
+    printf("Current threat level is:\n");
+    printThreat(myThreat);
+    printShoes(myThreat);
+    return 0;
 }

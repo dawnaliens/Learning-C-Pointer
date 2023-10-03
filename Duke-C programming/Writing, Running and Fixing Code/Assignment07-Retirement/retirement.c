29055c705daf5ea931a0c8d8e56c53ec3328800f
Richard Liu <richardliux23@gmail.com>
2023-10-04 05:58:01 +0800

    Not success

--- Duke-C programming/Writing, Running and Fixing Code/Assignment07-Retirement/retirement.c
+++ Duke-C programming/Writing, Running and Fixing Code/Assignment07-Retirement/retirement.c
@@ -1,23 +1,61 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct retire_info{
+// Define the retire_info struct
+typedef struct _retire_info {
     int months;
     double contribution;
     double rate_of_return;
-}retire_info;
+} retire_info;
 
+// Function to calculate retirement savings
+void retirement(int startAge, double initial, retire_info working, retire_info retired) {
+    int months = startAge;
+    double balance = initial;
 
-void retirement(int startAge, double initial, retire_info working, retire_info retired){
-    
+    for (int i = 0; i < working.months; i++) {
+        printf("Age %3d month %2d you have $%.2lf\n", months / 12, months % 12, balance);
+
+        // Calculate interest earned and add it to the balance
+        double interest = 1000 * (working.rate_of_return / 12);
+        balance += working.contribution + interest;
+
+        months++;
+    }
+
+    for (int i = 0; i < retired.months; i++) {
+        printf("Age %3d month %2d you have $%.2lf\n", months / 12, months % 12, balance);
+
+        // Calculate interest earned and add it to the balance
+        double interest = balance * (retired.rate_of_return / 12);
+        balance += retired.contribution + interest;
+
+        months++;
+    }
 }
 
+int main() {
+    int startAge = 327;  // Age in months (27 years and 3 months)
+    double initialSavings = 21345.0;
 
+    retire_info working_info = {489, 1000.0, 0.045 / 12};  // Working period info for 1 year
+    retire_info retired_info = {384, -4000.0, 0.01 / 12};  // Retirement period info
 
-int main(void){
-    int monthAge = 327;
-    int Age = monthAge / 12;
-    int month = monthAge % 12;
-    printf("Age = %d", Age);
-    printf("month = %d", month);
+    // printf("Working:\n");
+    // printf("Months: %d\n", working_info.months);
+    // printf("Per Month Savings: $%.2lf\n", working_info.contribution);
+    // printf("Rate of Return: %.2lf%% per year\n", working_info.rate_of_return * 12 * 100);
+
+    // printf("\nRetired:\n");
+    // printf("Months: %d\n", retired_info.months);
+    // printf("Per Month Spending: $%.2lf\n", retired_info.contribution);
+    // printf("Rate of Return: %.2lf%% per year\n", retired_info.rate_of_return * 12 * 100);
+
+    // printf("\nStarting conditions:\n");
+    // printf("Age: %d months (%d years, %d months)\n", startAge, startAge / 12, startAge % 12);
+    // printf("Savings: $%.2lf\n\n", initialSavings);
+
+    retirement(startAge, initialSavings, working_info, retired_info);
+
+    return 0;
 }
